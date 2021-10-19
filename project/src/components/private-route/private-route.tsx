d44e6090522d14c2eb194702a3ad123301b3a3de
Konstantin Sokolov <konstantin.sokolov@me.com>
2021-10-19 23:26:36 +0300

    Добавит тип PrivateRouteProps

--- project/src/components/private-route/private-route.tsx
+++ project/src/components/private-route/private-route.tsx
@@ -1,15 +1,26 @@
-import React from 'react';
-import {Route, Redirect} from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
+import { RouteProps } from 'react-router-dom';
+import { AppRoute, AuthorizationStatus } from '../../const';
 
-const hasAccess = false;
+type PrivateRouteProps = RouteProps & {
+  render: () => JSX.Element;
+  authorizationStatus: AuthorizationStatus;
+}
 
-const PrivateRoute = ({ component: Component, ...rest }) => (
-  <Route
-    {...rest}
-    render={(props) => {
-      return hasAccess ? <Component {...props} /> : <Redirect to="/login"/>;
-    }}
-  />
-);
+function PrivateRoute(props: PrivateRouteProps): JSX.Element {
+  const {exact, path, render, authorizationStatus} = props;
+
+  return (
+    <Route
+      exact={exact}
+      path={path}
+      render={() => (
+        authorizationStatus === AuthorizationStatus.Auth
+          ? render()
+          : <Redirect to={AppRoute.Login} />
+      )}
+    />
+  );
+}
 
 export default PrivateRoute;
